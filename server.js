713593487998a034eeff35ef0c136068f06462a1
Kevin Cowley <cowley.kevin.l@gmail.com>
2017-01-30 14:47:54 -0700

    CRUD fxn-ing

--- server.js
+++ server.js
@@ -3,11 +3,18 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
+
+
 const bodyParser = require('body-parser');
 const messages = require('./routes/classifieds');
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/../', 'node_modules')));
 
 app.use('/classifieds', messages);
 
